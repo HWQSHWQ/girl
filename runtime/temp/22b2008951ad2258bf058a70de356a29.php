9fd272ae848419a0cb64787a50d83347ede8097a
HWQSHWQ <1605312352@qq.com>
2019-03-14 16:07:40 +0800

    do

---
+++ runtime/temp/22b2008951ad2258bf058a70de356a29.php
@@ -0,0 +1,171 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:62:"D:\work\tp\public/../application/index\view\contact\index.html";i:1545878511;s:45:"D:\work\tp\application\index\view\header.html";i:1545876353;s:45:"D:\work\tp\application\index\view\footer.html";i:1545620035;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="renderer" content="webkit"><!-- 360开启急速模式！-->
+    <link rel="stylesheet" href="/uikit/css/uikit.min.css" />
+	
+</head>
+<body>
+<script  type="text/javascript" src="/uikit/js/jquery.min.js"></script>
+<script  type="text/javascript" src="/uikit/js/uikit.min.js"></script>
+<style>
+    .mainimg img{height:55px;width: 165px}
+	.header-img img{height:90%;width:100%}
+	.cooper-img img{width:100%}
+	
+</style>
+
+<!--导航栏-->
+<div style="">
+    <div style="display:inline-table;margin-left: 10%;" class="mainimg">
+        <nav class="uk-navbar">
+            <ul class="uk-navbar-nav">
+                <li class="uk-parent">
+                    <img src="/pic/1.png">
+                </li>
+            </ul>
+        </nav>	
+    </div>
+    <div style="display:inline-table;margin-left: 15%   ;background-color: white" class="mainimg">
+        <nav class="uk-navbar">
+            <ul class="uk-navbar-nav uk-nav-sub " style="background-color: white">
+                <li class="index"><a href="/index.php/index/index/index"><span class="">首页</span></a></li>
+                <li	class="company"><a href="/index.php/index/index/company">公司简介</a></li>
+                <li class="game"><a href="/index.php/index/index/game">游戏中心</a></li>
+                <li class="news"><a href="/index.php/index/index/news">新闻中心</a></li>
+                <li class="cooperation"><a href="/index.php/index/index/cooperation">商务合作</a></li>
+                <li class="contact"><a href="/index.php/index/index/contact">联系我们</a></li>
+            </ul>
+        </nav>
+
+    </div>
+</div>
+
+<!--为导航给选定样式-->
+<script>
+	 $("li.<?php echo $action; ?>").addClass('uk-active');
+</script>
+
+<link rel="stylesheet" href="/time/css/history.css" />
+<link rel="stylesheet" href="/uikit/css/components/slideshow.css" />
+<link rel="stylesheet" href="/uikit/css/components/dotnav.css" />
+<link rel="stylesheet" href="/uikit/css/components/slidenav.css" />
+<link rel="stylesheet" href="/uikit/css/components/slider.css" />
+
+<script  type="text/javascript" src="/uikit/js/components/slideshow.js"></script>
+<script  type="text/javascript" src="/uikit/js/components/slider.js"></script>
+<script  type="text/javascript" src="/time/js/main.js"></script>
+
+
+<script type="text/javascript" src="http://api.map.baidu.com/api?key=&v=1.1&services=true"></script>
+
+<div style="" class="uk-slidenav-position" data-uk-slideshow="">
+    <ul class="uk-slideshow " style="height: auto">
+		<li>
+			<img src="/pic/company.png" width="" height="" alt="">       
+        </li>  
+    </ul>
+</div>
+
+
+<div style="margin-top:20px;"  class="uk-grid">
+	<div class="uk-slidenav-position uk-width-1-6"></div>
+	<div class="uk-slidenav-position uk-width-4-6" >
+		<ul class="uk-tab" data-uk-tab>
+			<li class="uk-active"><a onclick="javascript:addRecord(1)">人才招聘</a></li>
+			<li><a onclick="javascript:addRecord(2)" >联系方式</a></li>
+		</ul>
+	</div>
+	<div class="uk-slidenav-position uk-width-1-6"></div>	
+</div>
+
+<div style="margin-top:15px;"  class="uk-grid">
+	<div class="uk-slidenav-position uk-width-1-6"></div>
+	<div class="uk-slidenav-position uk-width-4-6" >
+		<!--企业介绍 -->
+		<div id="id_1" class="uk-slider-container" style="display:block">
+			<img src="/pic/company_culture.png" width="" height="" alt=""> 
+			<div style="margin-top:10px">
+				<article class="uk-article">
+					<div class="uk-grid"> 
+						<div class="uk-slidenav-position uk-width-2-5"></div>
+						<span class="uk-text-large ">公司简介</span>
+					</div>
+					<p class="uk-article-meta">
+						余从京域，言归东藩，背伊阙 ，越轘辕，经通谷，陵景山。日既西倾，车殆马烦。尔乃税驾乎蘅皋，秣驷乎芝田，容与乎阳林，流眄乎洛川。于是精移神骇，忽焉思散。俯则未察，仰以殊观。睹一丽人，于岩之畔。乃援御者而告之曰：“尔有觌于彼者乎？彼何人斯，若此之艳也！”御者对曰：“臣闻河洛之神，名曰宓妃。然则君王所见，无乃是乎？其状若何，臣愿闻之。”
+					</p>
+				</article>
+			</div>	
+		</div>
+		
+		<!--企业文化 -->
+		<div id="id_2" class="uk-slider-container" style="display:none">
+			<div style="">
+				<article class="uk-article">
+					<p class="uk-article-meta">
+						广州荣端软件科技有限公司<br/>
+						人力资源部：郑小姐<br/>
+						座机：020-85200243<br/>
+						邮箱：zhengfangyan@rodgm.com<br/>
+						公司地址：广州市天河区龙口东路129号龙泽商业大厦1203A<br/>
+					</p><br/>
+					<iframe src="map.html" width="800px" height="500px" frameborder="0" scrolling="no"></iframe>
+					
+				</article>
+			</div>		
+		</div>
+		
+		
+	</div>
+	<div class="uk-slidenav-position uk-width-1-6"></div>	
+</div>	
+
+
+
+
+<script>
+	var addRecord = function(j){
+		var i;
+		for(i=1;i<=2;i++) {
+			if(i == j){$("#id_"+j).show();}
+			else $("#id_"+i).hide();
+		}
+		 
+	}
+	
+</script>
+
+
+<div class="footer uk-block uk-block-primary uk-contrast " style="background-color: white">
+    <div class="uk-container " style="margin-left: 15%">
+        <div class="uk-grid uk-grid-match" data-uk-grid-margin>
+            <div class="uk-width-medium-1-3 uk-row-first">
+                <div class="uk-panel mainimg">
+                    <img src="/pic/1.png">
+                </div>
+            </div>
+            <div class="uk-width-medium-1-3 uk-row-first uk-contrast">
+                <div class="uk-panel" style="color: black">
+                    <h3  style="color: black">联系我们</h3>
+                    <p>公司地址：XXXXXXXXXXXXXXXX</p>
+                    <p>联系电话：123456789</p>
+                    <p>电子邮箱：XXXXXXXXXXXXXXXXXX</p>
+                </div>
+            </div>
+            <div class="uk-width-medium-1-3 uk-row-first">
+                <div class="uk-panel"  style="color: black">
+                    <h3>&nbsp;</h3>
+                    <p>客服热线：123456789</p>
+                    <p>客服邮箱：XXXXXXXXXXXXXXXXXX</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ runtime/temp/595e97ea2841d1e6fd6a68b88940391e.php
@@ -0,0 +1,132 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:60:"D:\work\tp\public/../application/index\view\hello\index.html";i:1552464442;s:45:"D:\work\tp\application\index\view\footer.html";i:1545620035;}*/ ?>
+
+
+<link rel="stylesheet" href="/uikit/css/components/slideshow.css" />
+<link rel="stylesheet" href="/uikit/css/components/dotnav.css" />
+<link rel="stylesheet" href="/uikit/css/components/slidenav.css" />
+<link rel="stylesheet" href="/uikit/css/components/slider.css" />
+<script  type="text/javascript" src="/uikit/js/components/slideshow.js"></script>
+<script  type="text/javascript" src="/uikit/js/components/slider.js"></script>
+
+<div style="" class="uk-slidenav-position" data-uk-slideshow="{autoplay:false,kenburns:true}">
+    <ul class="uk-slideshow " style="height: auto">
+        <li>
+            <img src="/pic/2.png" width="" height="" alt="">
+            <div class="uk-overlay-panel uk-overlay-background uk-overlay-left">
+                <div style="margin-top:15%;margin-left: 80px">
+                    <h2 uk-heading-large>魔王血统</h2>
+                    <div style="margin-left: 20px">
+                        <p><font size="4">开启魔族狩猎之旅<br> <div>12月盛大公测</div></font></p>
+                    </div>
+                    <p style="margin-left:60px"><button class = "uk-button uk-button-success">查看详情  ></button></p>
+                </div>
+            </div>
+        </li>
+        <li><img src="/pic/company.png" width="" height="" alt=""></li>
+    </ul>
+    <a href="" class="uk-slidenav uk-slidenav-contrast uk-slidenav-previous" data-uk-slideshow-item="previous"></a>
+    <a href="" class="uk-slidenav uk-slidenav-contrast uk-slidenav-next" data-uk-slideshow-item="next"></a>
+    <ul class="uk-dotnav uk-dotnav-contrast uk-position-bottom uk-flex-center">
+        <li data-uk-slideshow-item="0"><a href=""></a></li>
+        <li data-uk-slideshow-item="1"><a href=""></a></li>
+    </ul>
+</div>
+
+<!--内容详情-->
+<div class="uk-grid" style="margin-top:20px;">
+    <div class="uk-width-medium-1-6 uk-panel-box-primary-hover ">
+    </div>
+    <div class="uk-width-medium-2-6 uk-panel-box-primary-hover  ">
+         <img src="/pic/4.png">
+    </div>
+    <div class="uk-width-medium-2-6 uk-panel-box-primary-hover" >
+        <div class="uk-grid">
+			<div class="uk-width-3-5" style="font-size:18px">&nbsp;最新消息</div>
+			<div class="uk-width-2-5" style="text-align:right;font-size:18px">更多++</div>
+			
+			<div class=" uk-width-3-5" style="margin-top:10px">【公司新闻】XXXXXXXXXXXXX</div>
+			<div class=" uk-width-2-5" style="margin-top:10px;text-align:right">2018-10-11</div>
+			
+			<div class=" uk-width-3-5">【公司新闻】XXXXXXXXXXXXX</div>
+			<div class=" uk-width-2-5" style="text-align:right">2018-10-11</div>
+			
+			<div class=" uk-width-3-5">【公司新闻】XXXXXXXXXXXXX</div>
+			<div class=" uk-width-2-5" style="text-align:right">2018-10-11</div>
+			
+			<div class=" uk-width-3-5">【公司新闻】XXXXXXXXXXXXX</div>
+			<div class=" uk-width-2-5" style="text-align:right">2018-10-11</div>
+        </div>
+    </div>
+    <div class="uk-width-medium-1-6 uk-panel-box-primary-hover ">
+    </div>
+</div>
+
+
+
+<!--游戏详情-->
+<div style="margin-top:20px;"  class="uk-grid">
+	<div class="uk-slidenav-position uk-width-1-6"></div>
+	<div class="uk-slidenav-position uk-width-4-6" data-uk-slider>
+		<div class="uk-slider-container">
+			<ul class="uk-slider uk-grid-width-medium-1-3">
+				<li><img src="/pic/5.png">
+					<span class="uk-text-large"><文明></span>
+					<br/>XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+					<br/>XXXXXXXXXXXXXXXXXXXXXXXXXXXXX		
+				</li>
+				<li><img src="/pic/5.png">
+					<span class="uk-text-large"><无尽></span>
+					<br/>XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+					<br/>XXXXXXXXXXXXXXXXXXXXXXXXXXXXX		
+				</li>
+				<li><img src="/pic/5.png">
+					<span class="uk-text-large"><魔王血统></span>
+					<br/>XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+					<br/>XXXXXXXXXXXXXXXXXXXXXXXXXXXXX		
+				</li>
+				<li><img src="/pic/5.png">
+					<span class="uk-text-large"><长生原></span>
+					<br/>XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+					<br/>XXXXXXXXXXXXXXXXXXXXXXXXXXXXX	
+				</li>
+			</ul>
+		</div>
+		<a href="#" style="margin-top:-60px" class="uk-slidenav uk-slidenav-contrast uk-slidenav-previous" data-uk-slider-item="previous" draggable="false"></a>
+		<a href="#" style="margin-top:-60px" class="uk-slidenav uk-slidenav-contrast uk-slidenav-next" data-uk-slider-item="next" draggable="false"></a>
+	</div>
+	<div class="uk-slidenav-position uk-width-1-6"></div>	
+</div>	
+
+
+
+
+<div class="footer uk-block uk-block-primary uk-contrast " style="background-color: white">
+    <div class="uk-container " style="margin-left: 15%">
+        <div class="uk-grid uk-grid-match" data-uk-grid-margin>
+            <div class="uk-width-medium-1-3 uk-row-first">
+                <div class="uk-panel mainimg">
+                    <img src="/pic/1.png">
+                </div>
+            </div>
+            <div class="uk-width-medium-1-3 uk-row-first uk-contrast">
+                <div class="uk-panel" style="color: black">
+                    <h3  style="color: black">联系我们</h3>
+                    <p>公司地址：XXXXXXXXXXXXXXXX</p>
+                    <p>联系电话：123456789</p>
+                    <p>电子邮箱：XXXXXXXXXXXXXXXXXX</p>
+                </div>
+            </div>
+            <div class="uk-width-medium-1-3 uk-row-first">
+                <div class="uk-panel"  style="color: black">
+                    <h3>&nbsp;</h3>
+                    <p>客服热线：123456789</p>
+                    <p>客服邮箱：XXXXXXXXXXXXXXXXXX</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
